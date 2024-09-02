d5dabb70de4522f3b67c7cc98eb367a2ae9b2bde
Arhum Javaid <92026550+arhumjavaid@users.noreply.github.com>
2024-09-02 22:47:08 +0500

    first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
+// import { FaLinkedin } from "react-icons/fa";
+// import { FaGithub } from "react-icons/fa";
+// import { FaInstagram } from "react-icons/fa";
+// import { FaFacebookF } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -13,11 +13,19 @@ const Navbar = () => {
           <h1 className="text-4xl font-bold">A</h1>
           <h1 className="text-4xl font-bold text-[#6161a9]">j</h1>
         </div>
-        <div className="m-8 flex justify-center items-center gap-4 text-2xl cursor-pointer">
-          <a href="http://www.linkedin.com/in/arhum-javaid-8b1807285" target="_blank"><FaLinkedin /></a>
-          <a href="https://github.com/arhumjavaid" target="_blank"><FaGithub /></a>
-          <FaInstagram />
-          <FaFacebookF />
+        <div className="links flex gap-10 text-neutral-200">
+          {["Services", "Our work", "About us", "Insights"].map(
+            (item, index) => (
+              <a
+                key={index}
+                className={`underline-effect text-[1.125rem] font-[400] capitalize cursor-pointer transform hover:translate-y-[-10%] transition-transform duration-300`}
+              >
+                <span className="overflow-hidden block relative">
+                  <span className="block">{item}</span>
+                </span>
+              </a>
+            )
+          )}
         </div>
       </nav>
     </>
