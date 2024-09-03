6b781f03bfdcd9556b02b765beb5ab60b934fe29
Arhum Javaid <92026550+arhumjavaid@users.noreply.github.com>
2024-09-03 14:23:43 +0500

    first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,15 +50,15 @@ const Navbar = () => {
       </nav>
 
       {showNav && (
-        <div className="fixed inset-0 w-[68%] bg-[#1A1A25] text-white z-50 p-8 flex flex-col gap-12">
-          <div className="flex flex-shrink-0 items-center gap-[2px] cursor-pointer mb-10">
+        <div className="fixed inset-0 w-[68%] bg-[#1A1A25] text-white z-50 p-8 flex flex-col gap-8">
+          <div className="flex flex-shrink-0 items-center gap-[2px] cursor-pointer mb-5">
             <h1 className="text-4xl font-bold">A</h1>
             <h1 className="text-4xl font-bold text-[#6161a9]">j</h1>
           </div>
           {["Home", "About", "Portfolio", "Contact"].map((item, index) => (
             <a
               key={index}
-              className="text-3xl font-light capitalize cursor-pointer hover:text-[#6161a9] transition-colors duration-300 "
+              className="text-2xl font-light capitalize cursor-pointer hover:text-[#6161a9] transition-transform duration-300 "
               onClick={() => setShowNav(false)}
             >
               {item}
